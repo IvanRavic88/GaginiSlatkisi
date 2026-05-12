a4f644115d8228bf095974fad888456acb5c64e9
IvanRavic88 <ravic.ivan88@gmail.com>
2026-05-12 11:37:54 +0200

    chore(lint): drop unused eslint-disable directives in JSON-LD components

--- components/seo/local-business-jsonld.tsx
+++ components/seo/local-business-jsonld.tsx
@@ -37,7 +37,6 @@ export async function LocalBusinessJsonLd() {
   return (
     <script
       type="application/ld+json"
-      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   )
--- components/seo/product-jsonld.tsx
+++ components/seo/product-jsonld.tsx
@@ -23,7 +23,6 @@ export function ProductJsonLd({ sweets }: { sweets: Sweet[] }) {
   return (
     <script
       type="application/ld+json"
-      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   )
